386efdfb298d0aefce651214f029fee829450360
scrabill <shannon.crabill@gmail.com>
2020-03-23 15:10:59 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = (props) =>
+  <div onClick={props.handleClick}>I am just happy.</div>;
+
+export default SimplerComponent;
+
+// How does this work with the click thing being undefined?!?!?!
